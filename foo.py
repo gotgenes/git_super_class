88e1558c17b9810a675c08b894fefaaaff7f66c9
Chris Lasher <clasher@5amsolutions.com>
2013-09-10 15:52:40 -0700

    Adds usage message.

--- foo.py
+++ foo.py
@@ -3,6 +3,10 @@
 
 import sys
 
+if len(sys.argv) < 2:
+    print "Usage: foo.py YOURNAME."
+    sys.exit()
+
 print "Hello, {}!".format(sys.argv[1])
 print "The sum of 2 and 3 is: {}".format(2 + 3)
 print "I like Git!"
